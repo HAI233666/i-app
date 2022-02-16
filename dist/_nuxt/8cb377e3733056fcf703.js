a110c53c8af7888658c9af0b0f18e699c1019bb1
tianqinghai <qhtian@qysoft.cn>
2022-02-16 17:30:31 +0800

    end.vue

---
+++ dist/_nuxt/8cb377e3733056fcf703.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{358:function(n,t,e){"use strict";e.r(t);var r={data:function(){return{newDate:new Date}}},c=e(21),component=Object(c.a)(r,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  "+this._s(this.newDate)+"\n")])},[],!1,null,null,null);t.default=component.exports}}]);
---
+++ pages/end.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-02-16 17:16:30
+ * @LastEditTime: 2022-02-16 17:17:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \i-app\pages\end.vue
+-->
+<template>
+  <div>
+    {{newDate}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newDate: new Date()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
